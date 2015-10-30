d18fec861d59e6d93a42eedea24f1054a268a4a1
Anis Abid <anis.abid-ext@amundi.com>
2015-10-30 17:19:47 +0100

    login

--- src/AccountsBundle/Controller/LoginController.php
+++ src/AccountsBundle/Controller/LoginController.php
@@ -9,8 +9,21 @@ use AA\PlatformBundle\Entity\Advert;
 
 class LoginController extends Controller
 {
-    public function indexAction()
+    public function loginAction(Request $request)
     {
-        return $this->render('AccountsBundle:Login:index.html.twig');
+        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('aa_platform_all');
+        }
+
+        // Le service authentication_utils permet de récupérer le nom d'utilisateur
+        // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
+        // (mauvais mot de passe par exemple)
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('AccountsBundle:Login:index.html.twig', array(
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        ));
     }
 }
