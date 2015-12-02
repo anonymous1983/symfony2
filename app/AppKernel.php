523cdd040966684a54d80051869d7dca2884f452
Anis Abid <anis.abid-ext@amundi.com>
2015-12-02 14:50:28 +0100

    delete account bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AA\PlatformBundle\AAPlatformBundle(),
-            new AccountsBundle\AccountsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/AccountsBundle/AccountsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AccountsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AccountsBundle extends Bundle
-{
-}
