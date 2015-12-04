7ab5d0838a9803cbc92feea4f863364f5508468a
Anis Abid <anis.abid-ext@amundi.com>
2015-12-04 16:04:42 +0100

    save

--- src/AA/PlatformBundle/Controller/AdvertRestController.php
+++ src/AA/PlatformBundle/Controller/AdvertRestController.php
@@ -4,7 +4,9 @@ namespace AA\PlatformBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use AA\PlatformBundle\Entity\Advert;
+use AA\PlatformBundle\Form\AdvertType;
 
 // use Symfony\component\HttpFoundation\JsonResponse;
 
@@ -28,4 +30,35 @@ class AdvertRestController extends Controller
 
         return $advert;
     }
+
+    public function postAdvertAction(Request $Request)
+    {
+        $advert = new Advert();
+        $form = $this->createForm(new AdvertType(), $advert);
+        $form->bind($request);
+
+        return $form;
+
+
+        /*$entity = $this->getDoctrine()->getManager();
+
+        $advert = new Advert();
+        $advert->setTitre("Mercedes classe c garantie 2017")
+            ->setDescription("suite mutation sur Paris je met en vente ma Mercedes classe c 180 exécutive garantie jusqu'en 2017 par Mercedes . la révision vient d'être effectuée . motorisation 156 chevaux turbo essence très faible consommation de 6litres \100,elle dispose d 'un grand réservoir de 66 litres(plus de 1000 kilomètres d'autonomie).de l'active park assist ( la voiture se gare toute seule) des feux a led i.l.s avec mode plein phares automatique .GPS Europe a commande vocale et pad tactile . bluetooth pour vos appels et votre musique . lecteur DVD et lecteur de carte sd plus 2 prises USB.capteur de stationnement avant et arrière. Stop and start automatique.aide au démarrage en côte.Régulateur et limiteur de vitesse avec le système anti collision ( la voiture freine automatiquement) capteur de panneaux de signalisation. Sièges en cuir chauffants et électriques avec soutien lombaires . rétroviseurs anti éblouissement etc..... Ma voiture est dans un état neuf aucune rayure ou choc et je suis Non fumeur ,elle sent toujours le neuf . photos supplémentaires sur demande.prix négociable raisonnablement")
+            ->setUrl("http://img1.leboncoin.fr/images/a03/a036940c7fa53e0b8ba988baf2f9836a192fc3c1.jpg")
+            ->setDate(new \DateTime());
+
+        $entity->persist($advert);
+        $entity->flush();
+
+        return $this->redirectView(
+                $this->generateUrl(
+                    'get_advert',
+                    array('id' => $entity->getId())
+                    ),
+                Codes::HTTP_CREATED
+                );*/
+    }
+
+
 }
---
+++ src/AA/PlatformBundle/Form/AdvertType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AA\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AdvertType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('url')
+            ->add('date')
+            ->add('status')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AA\PlatformBundle\Entity\Advert'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'aa_platformbundle_advert';
+    }
+}
