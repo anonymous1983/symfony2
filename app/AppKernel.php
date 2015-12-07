9ff26949bf0192e481dbf48358cdb19c5cfa05a6
Anis Abid <anis.abid-ext@amundi.com>
2015-12-07 16:48:37 +0100

    Fixed bug No 'Access-Control-Allow-Origin'

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AA\PlatformBundle\AAPlatformBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
